9e41de2801c8d4460af8fce8631a3e618f92da3c
Meet Shah <montu48@gmail.comm>
2020-04-08 14:40:06 +0530

    Clear and Delete ToDo

--- src/App.js
+++ src/App.js
@@ -33,10 +33,15 @@ class App extends Component {
     )
   };
   clearList = () => {
-    console.log("Clear List");
+   this.setState({
+     items:[]
+   })
   };
   handleDelete = (id) => {
-    console.log(id);
+    const filteredItems = this.state.items.filter(item => item.id !== id)
+    this.setState({
+      items:filteredItems
+    })
   };
   handleEdit = (id) => {
     console.log(id);
